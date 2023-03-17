f9687be363ee5001b6793f4a7713d14bc5e5b0dd
Jortizuwu <joseher2511@gmail.com>
2023-03-16 20:43:28 -0500

    frist commit

---
+++ polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
+
+polybar -rq one &
+polybar -rq title &
+polybar -rq two &
+
+
+
+echo "Polybar launched..."
